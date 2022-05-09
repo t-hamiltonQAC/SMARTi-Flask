5cbd4d37e5d0ff236dc7bd9cdc245e019b1fa587
Tom <thamilton@qaconsultants.com>
2022-05-09 14:24:14 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+
+from flask import Flask
+from flask import send_from_directory
+from flask import render_template
+
+
+app = Flask(__name__, )
+# app = Flask(__name__, template_folder='templates')
+
+FLUTTER_WEB_APP = 'templates'
+
+@app.route('/')
+def render_page():
+   # return "TESTING"
+    return render_template('index.html')
+
+
+@app.route('/web/')
+def render_page_web():
+    return render_template('index.html')
+
+
+@app.route('/web/<path:name>')
+def return_flutter_doc(name):
+
+    datalist = str(name).split('/')
+    DIR_NAME = FLUTTER_WEB_APP
+
+    if len(datalist) > 1:
+        for i in range(0, len(datalist) - 1):
+            DIR_NAME += '/' + datalist[i]
+
+    return send_from_directory(DIR_NAME, datalist[-1])
+
+
+if __name__ == '__main__':
+    app.run()
